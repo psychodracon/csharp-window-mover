fefac0875d2b955a9dffac20b4e7d99d8b427993
kganczarek <kamil.ganczarek@ttpsc.pl>
2021-10-12 16:11:32 +0200

    Initial commit of WPF version

---
+++ WindowMoverWPF/Data/ViewModel/AnchorsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowMover.Classes;
+using WindowMover.Classes.Managers;
+
+namespace WindowMoverWPF.Data.ViewModel
+{
+
+    class AnchorsViewModel
+    {
+        public ObservableCollection<WindowHandler> WindowHandlers { get; set; }
+        public AnchorsViewModel()
+        {
+            WindowHandlers = new ObservableCollection<WindowHandler>(WindowHandlerManager.windowHandlers);
+        }
+    }
+}
---
+++ WindowMoverWPF/Data/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowMover.Classes;
+using WindowMover.Classes.Managers;
+using WindowMoverWPF.Core;
+
+namespace WindowMoverWPF.Data.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand AnchorsViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        public AnchorsViewModel AnchorsViewModel { get; set; }
+        public SettingsViewModel SettingsViewModel { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { 
+                return _currentView;
+            }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public MainViewModel()
+        {
+            Settings.Instance.Load();
+            TimerManager.Mock();
+
+            AnchorsViewModel = new AnchorsViewModel();
+            SettingsViewModel = new SettingsViewModel();
+
+            AnchorsViewCommand = new RelayCommand(o => {
+                CurrentView = AnchorsViewModel;
+            });
+
+            SettingsViewCommand = new RelayCommand(o => {
+                CurrentView = SettingsViewModel;
+            });
+
+            CurrentView = AnchorsViewModel;
+        }
+    }
+}
